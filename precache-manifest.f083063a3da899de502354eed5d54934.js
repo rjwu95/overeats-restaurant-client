1d99d6196263460ba8d5e1eb80e85f487ec1acee
rjwu95 <yugun000@gmail.com>
2019-05-01 22:42:02 +0900

    Updates

---
+++ precache-manifest.f083063a3da899de502354eed5d54934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8627ad15ee1c4044610",
+    "url": "/restaurant/static/js/runtime~main.30357e72.js"
+  },
+  {
+    "revision": "0c869546cadeedbcb8ed",
+    "url": "/restaurant/static/js/main.44407d23.chunk.js"
+  },
+  {
+    "revision": "f385e1e08ea6106bccc8",
+    "url": "/restaurant/static/js/2.2a769c09.chunk.js"
+  },
+  {
+    "revision": "0c869546cadeedbcb8ed",
+    "url": "/restaurant/static/css/main.642e9986.chunk.css"
+  },
+  {
+    "revision": "499f78694590d3015fb3f6fb3b030fce",
+    "url": "/restaurant/index.html"
+  }
+];
